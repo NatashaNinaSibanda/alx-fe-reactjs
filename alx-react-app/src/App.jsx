ca894699621987bcd3b9cd1221618522dbef1cba
natasha sibanda <nsibanda374@gmail.com>
2025-09-05 21:01:57 +0200

    Add UserProfile card components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,15 @@
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import React from 'react';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1>User Profiles</h1>
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <UserProfile name="Bob" age={30} bio="Enjoys coding and traveling" />
+      <UserProfile name="Natasha" age={22} bio="Aspiring software engineer" />
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;

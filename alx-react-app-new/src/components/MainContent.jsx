2b9dc9c8d9c31110246edcce1b6e665dad1d00f3
natasha sibanda <nsibanda374@gmail.com>
2025-09-15 01:54:58 +0200

    Add the paragraph on MainContent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -5,7 +5,7 @@ const MainContent = () => {
     <main style={{ padding: "20px", backgroundColor: "#f0f0f0", color: "#333", minHeight: "200px" }}>
       <h2 style={{ textAlign: "center", color: "darkgreen" }}>Welcome to My Page</h2>
       <p style={{ lineHeight: "1.6", marginTop: "10px" }}>
-        I love to visit New York, PAris, and Tokyo.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );

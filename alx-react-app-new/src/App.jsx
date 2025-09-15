9676ee0a07458981426706099b2b34b7831fe84b
natasha sibanda <nsibanda374@gmail.com>
2025-09-15 02:09:52 +0200

    Add Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,12 +3,14 @@ import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <Counter /> 
       <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
         <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
         <UserProfile name="Bob" age={30} bio="Enjoys cooking and traveling" />
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      {/* Counter content will go here */}
+    </div>
+  );
+};
+
+export default Counter;

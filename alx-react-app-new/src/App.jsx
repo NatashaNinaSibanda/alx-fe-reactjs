26d1a7021869828ab3411b28df39fd512d21f3b3
natasha sibanda <nsibanda374@gmail.com>
2025-09-16 19:46:06 +0200

    Add counter component and update app.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
+
       <Header />
       <MainContent />
-      <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", marginTop: "20px" }}>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        <Counter />
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          marginTop: "20px",
+        }}
+      >
         <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
         <UserProfile name="Bob" age={30} bio="Enjoys cooking and traveling" />
         <UserProfile name="Charlie" age={28} bio="Fan of music and tech gadgets" />
       </div>
       <Footer />
-      <Counter />
     </div>
   );
 }
 
 export default App;
+
 
 

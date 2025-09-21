bb75823fd44e6e0cb4cdcacee2f2c0956b568b44
natasha sibanda <nsibanda374@gmail.com>
2025-09-22 00:20:33 +0200

    Wrap App.jsx with Router for routing implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,33 +1,36 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div>
-      <h1>🍲 Recipe Sharing App</h1>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
+    <Router>
+      <div>
+        <h1>🍲 Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
 
 
         

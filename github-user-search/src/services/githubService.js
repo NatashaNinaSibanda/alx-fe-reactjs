5389bd815e4f6d3c729f0eab10e54848fac349af
natasha sibanda <nsibanda374@gmail.com>
2025-09-26 04:06:05 +0200

    Fix: Import axios directly in githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,18 @@
-import api from "./githubApi"; 
+import axios from "axios"; 
+
+const GITHUB_API_URL = "https://api.github.com/users";
+const TOKEN = import.meta.env.VITE_APP_GITHUB_API_KEY || "";
 
 export const fetchUserData = async (username) => {
   try {
-    const response = await api.get(`/users/${username}`);
+    const response = await axios.get(`${GITHUB_API_URL}/${username}`, {
+      headers: {
+        Authorization: TOKEN ? `token ${TOKEN}` : "",
+      },
+    });
     return response.data;
   } catch (error) {
     throw error;
   }
 };
+

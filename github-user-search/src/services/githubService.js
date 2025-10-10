2201ec5c54f8540c106fa77604e3258f9af8b7ac
natasha sibanda <nsibanda374@gmail.com>
2025-10-10 03:44:17 +0200

    Add the new github service

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,27 +1,21 @@
 import axios from "axios";
 
-// Base URL for GitHub API
-const BASE_URL = "https://api.github.com/search/users";
 
-/**
- * Search GitHub users with advanced filters.
- * @param {string} username - The username to search for.
- * @param {string} location - The location filter.
- * @param {string|number} minRepos - Minimum repository count filter.
- * @returns {Promise<Array>} A list of matching users.
- */
 export const searchUsers = async (username, location, minRepos) => {
   try {
-    // Build the query string
+  
     let query = "";
 
     if (username) query += `${username} in:login`;
     if (location) query += ` location:${location}`;
     if (minRepos) query += ` repos:>${minRepos}`;
 
-    const response = await axios.get(`${BASE_URL}?q=${query}`);
 
-    // Return simplified user data
+    const url = `https://api.github.com/search/users?q=${query}`;
+
+    const response = await axios.get(url);
+
+  
     return response.data.items.map((user) => ({
       id: user.id,
       login: user.login,
@@ -33,6 +27,7 @@ export const searchUsers = async (username, location, minRepos) => {
     throw error;
   }
 };
+
 
 
 
